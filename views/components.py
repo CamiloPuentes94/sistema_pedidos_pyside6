f43d45093b8cb2ec820d9a2ab5f89aa63e04efb1
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2022-02-02 08:57:33 -0500

    Se crea clase para cargar imagen de los archivos cargados o creados

--- views/components.py
+++ views/components.py
@@ -1,2 +1,12 @@
 from PySide6.QtWidgets import QLabel
 from PySide6.QtGui import QPixmap
+
+class RecipeImg(QLabel):
+    
+    def __init__(self, img_path):
+        super().__init__()
+        
+        # con esta variable cargamos la imgen con el path y le damos el tamaño a la imagen
+        self.img = QPixmap(img_path).scaledToWidth(200)
+        #con esta funcion ponemos visualizar la imagen
+        self.setPixmap(self.img)
