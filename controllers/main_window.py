186fa4a94ebb7739e944b4dcabd97583f378c921
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2022-02-02 09:09:59 -0500

    Se agrega funcion para visualizar tabla en la aplicacion de lo que contiene la base datos, y se crea otra funcion para colocarles nombres a las columnas y poder visualizar informacion

--- controllers/main_window.py
+++ controllers/main_window.py
@@ -1,9 +1,11 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QTableWidgetItem
 
 from views.main_window import MainWindow
 from views.general_custom_ui import GeneralCustomUi
 from controllers.add_window import AddWindowForm
+from views import components
 
+from db import recipes
 
 class MainWindowForm(QWidget, MainWindow):
     
@@ -11,6 +13,9 @@ class MainWindowForm(QWidget, MainWindow):
         super().__init__()
         self.setupUi(self)
         self.ui = GeneralCustomUi(self)
+        
+        self.config_table()
+        self.populate_table()
         
         self.new_recipe_button.clicked.connect(self.open_add_window)
         
@@ -21,3 +26,34 @@ class MainWindowForm(QWidget, MainWindow):
     def open_add_window(self):
         window = AddWindowForm()
         window.show()
+     
+    def config_table(self):
+        # en esta variable colocamos el nombre de las columnas a visualizar
+        colum_labels = ("ID", "IMG", "TITULO", "CATEGORIA")
+        # Se envian la cantidad de columnas que va a tener
+        self.recipes_table.setColumnCount(len(colum_labels))
+        #Se envian lo labels de la columna
+        self.recipes_table.setHorizontalHeaderLabels(colum_labels)
+     
+        
+    def populate_table(self):
+        data = recipes.select_all()
+        
+        self.recipes_table.setRowCount(len(data))
+        
+        # este for se capturan los datos de las filas
+        for (index_row, row) in enumerate(data):
+            # en este for se captura los datos de cada celda
+            for (index_cell, cell) in enumerate(row):
+                if index_cell == 1:
+                    self.recipes_table.setCellWidget(
+                        index_row,
+                        index_cell,
+                        components.RecipeImg(cell)
+                    )
+                else:
+                    self.recipes_table.setItem(
+                        index_row,
+                        index_cell,
+                        QTableWidgetItem(str(cell))
+                    )
