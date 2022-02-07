ef3e22343fa8189120af4160211f9b57c65e2613
Juan Camilo Puentes Sandoval <jcpsandoval94@gmail.com>
2022-02-07 16:50:35 -0500

    Se agrega funcion para cambio de curso cuando este en el objeto selecionado

--- views/components.py
+++ views/components.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QLabel, QPushButton
-from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtGui import QPixmap, QIcon, QCursor
+from PySide6.QtCore import Qt
 
 class RecipeImg(QLabel):
     
@@ -17,10 +18,13 @@ class Button(QPushButton):
     def __init__(self, icon, color):
         # funcion para tamaño de boton
         self.setMinimumSize(30, 30)
+        self.set_cursor()
         # funcion para ponerles un icono
         self.setIcon(QIcon(f"assets/icons/{icon}.png"))
         # funcion para cambiar color o cambiar estilo
         self.setStyleSheet(f"border-radius: 15px; background-color {color};")
         
-    
-        
+    # funcion para cambie el cursor cuando este en el boton
+    def set_cursor(self):
+        pointer = QCursor(Qt.PointingHandCursor())
+        self.setCursor(pointer)
